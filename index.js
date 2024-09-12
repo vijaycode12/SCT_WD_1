f0e5ca36ffb53134ca88dcd863b90b0efd9ecdde
vijaycode12 <reddyvijay4043@gmail.com>
2024-09-12 21:04:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const navLinks = document.querySelectorAll('a');
+const sections = document.querySelectorAll('section');
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        navLinks.forEach(link => link.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+window.addEventListener('scroll', () => {
+    let currentSection = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${currentSection}`) {
+            link.classList.add('active');
+        }
+    });
+});
